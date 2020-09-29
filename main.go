71d2a9cf1bb3d263e8598b216e8eea64d0a8bf7d
PWho <patrick@patrickwho.me>
2020-09-29 12:49:01 -0400

    Add basic main.go to get things going
    I plan on using GoKit, but I just have
    some of those basics stubbed in at the
    moment. At this point, I know my server
    is working.
    
    I spent a bit of time looking into setting
    up a true secure connection locally,
    but I ran into issues with not being
    able to bind to the :443 or :80 ports
    with ListenAndServeTLS. From what I could
    find online, it looks like Linux disallows
    binding to these ports with sudo or some
    other capabilities. It looked like
    a bit of a hassle to get this working
    locally, so I decided to skip this for
    now and get to the fun stuff of setting
    up the web-scraper.

---
+++ main.go
@@ -0,0 +1,38 @@
+package main
+
+import (
+	// "encoding/json"
+	"flag"
+	// "fmt"
+	"log"
+	"net/http"
+	"time"
+
+	"github.com/go-chi/chi"
+	// "github.com/go-kit/kit/endpoint"
+	// httptransport "github.com/go-kit/kit/transport/http"
+)
+
+func main() {
+
+	var (
+		domain = flag.String("domain", ":8080", "Top Level Domain")
+	)
+
+	flag.Parse()
+
+	r := chi.NewRouter()
+
+	r.Get("/", func(w http.ResponseWriter, r *http.Request) {
+		w.Write([]byte("Hello, World"))
+	})
+
+	srv := &http.Server{
+		ReadTimeout:  5 * time.Second,
+		WriteTimeout: 5 * time.Second,
+		Handler:      r,
+		Addr:         *domain,
+	}
+
+	log.Fatal(srv.ListenAndServe())
+}
